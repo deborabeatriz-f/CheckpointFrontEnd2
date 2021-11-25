8747ee9517974db962b1bf6366e82c54ded95e5b
Débora Beatriz Figueiró <82326298+deborabeatriz-f@users.noreply.github.com>
2021-11-25 16:27:10 -0300

    Update JS

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ let tarefa = document.getElementById("tarefa");
 let dataCriacao = document.getElementById("dataCriacao");
 let data = document.getElementById("data");
 
-dataCriacao.value = new Date().toISOString().slice(0, 10)
+dataCriacao.value = new Date().toISOString().slice(0, 10);
+
+let lista = document.querySelector("ul");
 
 // Botão
 btn.addEventListener("click", (event) => {
-    event.preventDefault();
-};
+  event.preventDefault();
+
+  let listaTarefas = document.createElement("li");
+
+  listaTarefas.innerText = `Tarefa: ${tarefa.value}. Criação da tarefa: ${dataCriacao}. Previsão de finalização da tarefa: ${data}.`;
+
+  lista.appendChild(listaTarefas);
+});
