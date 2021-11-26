c9b45c8113017298812a184b847fbe71d1db2f25
Denny Ribeiro <denny.ribeiro@outlook.com>
2021-11-26 20:16:18 -0300

    adicionando descrição + validação dos campos

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@ let btn = document.querySelector("#adicionar");
 let tarefa = document.getElementById("tarefa");
 let dataCriacao = document.getElementById("dataCriacao");
 let data = document.getElementById("data");
-
+let lista = document.querySelector("ul");
+let desc = document.getElementById('desc')
 dataCriacao.value = new Date().toISOString().slice(0, 10);
 
-let lista = document.querySelector("ul");
 
-// Botão
+// Adicionando tarefas
 btn.addEventListener("click", (event) => {
   event.preventDefault();
 
-  let listaTarefas = document.createElement("li");
+  // validando os campos
+  if (desc.value.length < 10) {
+    return alert('A descrição deve ter no mínimo 10 caracteres')
+  } else if (tarefa.value.length == 0) {
+    return alert("O campo 'tarefa' não pode ficar vazio.")
+  } else if (data.value == "") {
+    return alert('Favor, preencher a data')
+  } else {
 
-  listaTarefas.innerText = `Tarefa: ${tarefa.value}. Criação da tarefa: ${dataCriacao.value}. Previsão de finalização da tarefa: ${data.value}.`;
+    let listaTarefas = document.createElement("li");
 
-  lista.appendChild(listaTarefas);
-});
+    listaTarefas.innerHTML = `Tarefa: ${tarefa.value}. Criação da tarefa: ${dataCriacao.value}. Previsão de finalização da tarefa: ${data.value}. <i class="fas fa-trash"></i>`;
+
+    lista.appendChild(listaTarefas);
+  }
+})
+
+
