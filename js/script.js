8978562d3bcad606b0ad76c768afa23db9c77cc3
EctorCunha <ectorvccunha@outlook.com>
2021-11-27 14:55:41 -0300

    Temas
    add os temas

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-let formulario = document.querySelector('.nova-tarefa');
-let btn = document.querySelector('#adicionar');
-let tarefa = document.getElementById('tarefa');
-let dataCriacao = document.getElementById('dataCriacao');
-let data = document.getElementById('data');
-let lista = document.querySelector('ul');
-let desc = document.getElementById('desc');
-dataCriacao.value = new Date().toISOString().slice(0, 10);
+let formulario = document.querySelector('.nova-tarefa')
+let btn = document.querySelector('#adicionar')
+let tarefa = document.getElementById('tarefa')
+let dataCriacao = document.getElementById('dataCriacao')
+let data = document.getElementById('data')
+let lista = document.querySelector('ul')
+let desc = document.getElementById('desc')
+dataCriacao.value = new Date().toISOString().slice(0, 10)
 
 // Dados da aplicação
 const estado = {
-  tarefas: [],
-};
+  tarefas: []
+}
 
 // Função que será chamada sempre que uma tarefa for adicionada ou removida do estado
 function renderizarTarefas() {
   if (estado.tarefas.length === 0) {
-    lista.innerHTML = '<p>Não há tarefas</p>';
+    lista.innerHTML = '<p>Não há tarefas</p>'
   }
   lista.innerHTML = estado.tarefas.map(
-    (tarefa) => `
+    tarefa => `
       <li>
         <div class="card" data-id="${tarefa.id}">
           <p>Tarefa: ${tarefa.titulo}</p>
@@ -31,14 +31,14 @@ function renderizarTarefas() {
         </div>
       </li>
     `
-  );
+  )
 }
 
 // Event handler: Envio do formulário
 function adicionarTarefa(event) {
-  event.preventDefault();
+  event.preventDefault()
 
-  if (!validarCampos()) return;
+  if (!validarCampos()) return
 
   const novaTarefa = {
     id: Date.now(),
@@ -46,50 +46,76 @@ function adicionarTarefa(event) {
     dataCriacao: dataCriacao.value,
     dataLimite: data.value,
     descricao: desc.value,
-    completado: false,
-  };
+    completado: false
+  }
 
-  estado.tarefas.push(novaTarefa);
-  limparFormulario();
-  renderizarTarefas();
+  estado.tarefas.push(novaTarefa)
+  limparFormulario()
+  renderizarTarefas()
 }
 
 function removerTarefa(event) {
-  if (!event.target.closest('.remover-tarefa')) return;
+  if (!event.target.closest('.remover-tarefa')) return
 
-  const confirmarExclusao = confirm('Deseja mesmo excluir a tarefa?');
-  if (!confirmarExclusao) return;
+  const confirmarExclusao = confirm('Deseja mesmo excluir a tarefa?')
+  if (!confirmarExclusao) return
 
-  const tarefaId = event.target.closest('.card').dataset.id;
-  const tarefaIndex = estado.tarefas.findIndex((tarefa) => tarefa.id == tarefaId);
+  const tarefaId = event.target.closest('.card').dataset.id
+  const tarefaIndex = estado.tarefas.findIndex(tarefa => tarefa.id == tarefaId)
 
-  estado.tarefas.splice(tarefaIndex);
-  renderizarTarefas();
+  estado.tarefas.splice(tarefaIndex)
+  renderizarTarefas()
 }
 
 // Função utilitária: Limpa os campos do formulário
 function limparFormulario() {
-  tarefa.value = '';
-  data.value = '';
-  desc.value = '';
+  tarefa.value = ''
+  data.value = ''
+  desc.value = ''
 }
 
 // Função utilitária: Valida os campos do formulário
 function validarCampos() {
   if (desc.value.length < 10) {
-    alert('A descrição deve ter no mínimo 10 caracteres');
-    return false;
+    alert('A descrição deve ter no mínimo 10 caracteres')
+    return false
   } else if (tarefa.value.length == 0) {
-    alert("O campo 'tarefa' não pode ficar vazio.");
-    return false;
+    alert("O campo 'tarefa' não pode ficar vazio.")
+    return false
   } else if (data.value == '') {
-    alert('Favor, preencher a data');
-    return false;
+    alert('Favor, preencher a data')
+    return false
   }
-  return true;
+  return true
 }
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', renderizarTarefas);
-formulario.addEventListener('submit', adicionarTarefa);
-lista.addEventListener('click', removerTarefa);
+document.addEventListener('DOMContentLoaded', renderizarTarefas)
+formulario.addEventListener('submit', adicionarTarefa)
+lista.addEventListener('click', removerTarefa)
+
+// Temas
+let branco = document.getElementById('branco')
+let amareloEscuro = document.getElementById('amareloEscuro')
+let azulTurquesa = document.getElementById('azulTurquesa')
+let dark = document.getElementById('dark')
+
+branco.onclick = function () {
+  let tema = document.getElementsByTagName('link')[0]
+  tema.setAttribute('href', './css/styleBranco.css')
+}
+
+amareloEscuro.onclick = function () {
+  let tema = document.getElementsByTagName('link')[0]
+  tema.setAttribute('href', './css/styleAmareloEscuro.css')
+}
+
+azulTurquesa.onclick = function () {
+  let tema = document.getElementsByTagName('link')[0]
+  tema.setAttribute('href', './css/styleAzulTurquesa.css')
+}
+
+dark.onclick = function () {
+  let tema = document.getElementsByTagName('link')[0]
+  tema.setAttribute('href', './css/styleDark.css')
+}
